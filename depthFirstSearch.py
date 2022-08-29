b315ba654a3da4a4cac0674ac133e7c90bba57ce
Vance Cole <vancewcole@gmail.com>
2022-08-28 19:56:17 -0500

    Graph time

---
+++ depthFirstSearch.py
@@ -0,0 +1,30 @@
+#https://favtutor.com/blogs/depth-first-search-python
+
+# Using a Python dictionary to act as an adjacency list
+# graph = {
+#   '5' : ['3','7'],
+#   '3' : ['2', '4'],
+#   '7' : ['8'],
+#   '2' : [],
+#   '4' : ['8'],
+#   '8' : []
+# }
+
+graph = {
+    '1': ['1','2','3'],
+    '2': ['1','2','3'],
+    '3': ['1','2','3']
+}
+
+visited = set() # Set to keep track of visited nodes of graph.
+
+def dfs(visited, graph, node):  #function for dfs 
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+# Driver Code
+print("Following is the Depth-First Search")
+dfs(visited, graph, '2')
